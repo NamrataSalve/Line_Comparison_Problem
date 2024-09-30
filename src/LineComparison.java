10b92b232801f2a766c38d3753a7ed977282b5d9
NamrataSalve <namratasalve1999@gmail.com>
2024-09-30 12:29:55 +0530

    [Namrata] Added: Compare two lines based on end points

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -31,8 +31,12 @@ public class LineComparison {
         System.out.println("Length of given Line having points "+"("+_x1+","+_y1+")"+"  "+"("+_x2+","+_y2+")"+" is: "+len2);
         String l1 = Double.toString(len1);
         String l2 = Double.toString(len2);
-        boolean equal = l1.equals(l2);
-        System.out.println("Equality of two lines : "+equal);
+        if (l1.compareTo(l2)>0)
+            System.out.println("Line1 is longer than line2");
+        else if (l1.compareTo(l2)==0)
+            System.out.println("Line1 is equal to line2");
+        else
+            System.out.println("Line2 is longer than line1");
     }
     public static double getLengthLine1(int x1, int y1, int x2, int y2){
         return Math.sqrt(((x2 - x1)*(x2 - x1)) + ((y2- y1)*(y2- y1)));
