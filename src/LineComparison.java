21e6bb2bd48e3a285acf9c6a166e6824240facec
NamrataSalve <namratasalve1999@gmail.com>
2024-09-30 11:52:44 +0530

    [Namrata] Added: Code for welcome masage

---
+++ src/LineComparison.java
@@ -0,0 +1,9 @@
+/*
+All code for finding length or check equality and line comparison
+27-sep-2024
+ */
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
